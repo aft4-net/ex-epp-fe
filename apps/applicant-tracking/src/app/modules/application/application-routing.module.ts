5b39360de8593e6c407af45e00dcb212bfd41701
Kal <Kal@DESKTOP-L318E7J>
2021-10-25 20:53:09 +0300

    added application sider menu with routing

--- apps/applicant-tracking/src/app/modules/application/application-routing.module.ts
+++ apps/applicant-tracking/src/app/modules/application/application-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SiderComponent } from '../../components/application/sider/sider.component';
+import { PersonalInformationComponent } from '../../components/application/personal-information/personal-information.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SiderComponent,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'personal-info' },
+      {
+        path: 'personal-info',
+        component: PersonalInformationComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ApplicationRoutingModule { }
+export class ApplicationRoutingModule {}
