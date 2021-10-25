239e5894dcc8ebeeb085cac12afe2ea75234c338
fikremariam <fmoges@excellerentsolutions.com>
2021-10-25 17:21:33 +0300

    added login form validation on email and password fields

--- apps/applicant-tracking/src/app/components/user/signin/signin.component.ts
+++ apps/applicant-tracking/src/app/components/user/signin/signin.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
+  Validators,
 } from '@angular/forms';
 
 @Component({
@@ -12,14 +14,25 @@ import {
 export class SigninComponent implements OnInit {
   showPassword: boolean = false;
   loginForm = new FormGroup({
-    email: new FormControl('', []),
-    password: new FormControl('', []),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
   });
+  get loginEmail(): AbstractControl | null {
+    return this.loginForm.get('email');
+  }
+  get loginPassword(): AbstractControl | null {
+    return this.loginForm.get('password');
+  }
 
   login() {}
   togglePasswordView() {
     this.showPassword = !this.showPassword;
   }
+
+  constructor() {}
 
   ngOnInit(): void {}
 }
