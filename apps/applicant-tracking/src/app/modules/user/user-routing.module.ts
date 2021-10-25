c4de0ae8f9b24424c42812f1f9a44b1e4db82d08
fikremariam <fmoges@excellerentsolutions.com>
2021-10-25 17:06:16 +0300

    added login component ui

--- apps/applicant-tracking/src/app/modules/user/user-routing.module.ts
+++ apps/applicant-tracking/src/app/modules/user/user-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from '../../components/user/signup/signup.component';
+import { SigninComponent } from '../../components/user/signin/signin.component';
 
-const routes: Routes = [{ path: 'signup', component: SignupComponent }];
+const routes: Routes = [
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
