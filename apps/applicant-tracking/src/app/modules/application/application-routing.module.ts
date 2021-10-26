faa11b577cbe82d89c3a305450aa0e8a101816f4
wolde <woldeyalew04@gmail.com>
2021-10-26 09:56:59 +0300

    back angular.json defualt passport

--- apps/applicant-tracking/src/app/modules/application/application-routing.module.ts
+++ apps/applicant-tracking/src/app/modules/application/application-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonalInformationComponent } from '../../components/application/personal-information/personal-information.component';
-const routes: Routes = [{ path: 'personalinformation', component: PersonalInformationComponent }];
+const routes: Routes = [{ path: 'personal-information', component: PersonalInformationComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
