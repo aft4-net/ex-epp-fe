77e918819054db1c15c71ac69a1d180f6d2a889e
Berihun Hadis <bhadis@excellerentsolutions.com>
2021-10-27 18:09:40 +0300

    method name renamed per the comment

--- apps/timesheet/src/app/timesheet/services/day-and-date.service.ts
+++ apps/timesheet/src/app/timesheet/services/day-and-date.service.ts
@@ -98,66 +98,7 @@ export class DayAndDateService {
     return this.firstday1;
   }
 
-  myFunctionWeek0(cuur1: any, decrease: any): any[] {
-    if (cuur1 != null) {
-      this.weekDays = [];
-      this.lastday = '';
-      this.firstday = '';
-      this.lastday1 = '';
-      this.firstday = new Date(cuur1.setDate(cuur1.getDate() - cuur1.getDay()))
-      this.firstday1 = new Date(cuur1.setDate(cuur1.getDate() - cuur1.getDay() - 6));
-      // this.firstday1 = null;
-      for (let i = 0; i >= -8; i--) {
-        if (this.weekDays.length === 7) {
-          break
-        } else {
-          if (i === 0) {
-            continue;
-          } else {
-            this.lastday = new Date(cuur1.setDate(cuur1.getDate() - cuur1.getDay() - i));
-            let bb: DaydateModel;
-            // eslint-disable-next-line prefer-const
-            bb = this.lastday;
-            this.weekDays.push(bb);
-            this.lastday1 = bb;
-            // if (this.firstday1 == null) {
-            //   this.firstday1 = this.lastday;
-            // }
-          }
-        }
-      }
-    }
-    return this.weekDays;
-  }
-
-  myFunctionWeek1(cuur1: any, inc: any): any[] {
-
-    this.weekDays = [];
-    this.lastday = '';
-    this.firstday = '';
-    this.lastday1 = '';
-    this.firstday = new Date(cuur1.setDate(cuur1.getDate() - cuur1.getDay()))
-    this.firstday1 = new Date(cuur1.setDate(cuur1.getDate() - cuur1.getDay() + 1));
-    // this.firstday1 = null;
-    for (let i = 0; i <= 8; i++) {
-      if (this.weekDays.length === 7) {
-        break
-      } else {
-        if (i === 0) {
-          continue;
-        } else {
-          this.lastday = new Date(cuur1.setDate(cuur1.getDate() - cuur1.getDay() + i));
-          let bb: DaydateModel;
-          // eslint-disable-next-line prefer-const
-          bb = this.lastday;
-          this.weekDays.push(bb);
-          this.lastday1 = bb;
-        }
-      }
-    }
-    return this.weekDays;
-  }
-
+  
   weekByDate(cuur1: any): any[] {
     if (cuur1 != null) {
       this.weekDays = [];
