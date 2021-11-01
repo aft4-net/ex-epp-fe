7eff2631fbb2e0c39413a0adafd17b5e64fb8282
Abrham-Yifter <88376753+Abrham-Yifter@users.noreply.github.com>
2021-11-02 00:48:02 +0300

    added sidebar auth sample and notification for success and errors

--- apps/applicant-tracking/src/app/services/navigationGuard/aoiGuard.ts
+++ apps/applicant-tracking/src/app/services/navigationGuard/aoiGuard.ts
@@ -1,25 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { PersonalInformation } from '../../models/personal-information';
+import { NotificationBar } from '../../utils/feedbacks/notification';
 import { AccountService } from '../user/account.service';
 
 @Injectable({ providedIn: 'root' })
 export class aoiGuard implements CanActivate {
-    constructor(private router: Router, private accountService: AccountService) {}
-    generalInfo = {} as PersonalInformation[];
+    constructor(private router: Router, private accountService: AccountService, private notification: NotificationBar,) {}
+    generalInfo = {} as PersonalInformation;
     profile = <PersonalInformation>{}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const user = this.accountService.userInfo;
 
-            this.accountService.generalInfo(user.email).subscribe(res => {
-              this.generalInfo = res.Data[0];
-              this.profile = this.generalInfo[0];
-              if (this.profile.phoneNo) {
-                  this.router.navigateByUrl('application/personal-info');
-                  return false
+            this.accountService.generalInfo(user.Email).subscribe(res => {
+              this.generalInfo = res.Data;
+              this.profile = this.generalInfo;
+              if (!this.profile.phoneNo) {
+                  this.router.navigateByUrl('application/personal-information');
+                  this.notification.showNotification({
+                    type: 'error',
+                    content: 'Personal information form should be filled in first!!',
+                    duration: 5000,
+                  });
+                  return false;
                 }
+                return false;
             }, error =>{
               console.log(error);
+              this.router.navigateByUrl('application/personal-information');
+              this.notification.showNotification({
+                type: 'error',
+                content: error,
+                duration: 5000,
+              });
+              return false;
             });
         return true;
     }
