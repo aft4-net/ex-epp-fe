8284b25e8810d03b74eb5bc1960823eac60528d8
Isyas Tadese <isayasone@outllook.com>
2021-11-05 19:14:12 +0300

    layout-edited

---
+++ apps/project-management/src/app/core/services/employee.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Employee } from '@exec-epp/core-models';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+
+  APIUrl="http://localhost:3333/api/Employees "; 
+  constructor(protected httpClient: HttpClient ) {
+  }
+
+  getAll(): Observable<Array<Employee>>
+  {
+    return this.httpClient.get<Employee[]>(this.APIUrl);
+  }
+
+}
