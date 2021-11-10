2760a578ff793d3bf6d27a2867a2eb752ef6cb9c
yosef kefale <yoseph.kefale@gmail.com>
2021-11-10 15:28:51 +0300

    fixes after conflict

--- apps/project-management/src/app/features/project/components/project-detail/project-detail.component.ts
+++ apps/project-management/src/app/features/project/components/project-detail/project-detail.component.ts
@@ -3,7 +3,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
 
@@ -14,6 +14,11 @@ import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
 })
 export class ProjectDetailComponent implements OnInit {
   validateForm!: FormGroup;
+  userSubmitted!: boolean;
+  currentDate = Date.now.toString();
+
+  @ViewChild('endDatePicker') endDatePicker!: NzDatePickerComponent;
+  @ViewChild('startDatePicker') startDatepicker!: NzDatePickerComponent;
 
   submitForm(): void {
     for (const i in this.validateForm.controls) {
@@ -25,12 +30,17 @@ export class ProjectDetailComponent implements OnInit {
   }
 
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    console.log(this.currentDate)
+    this.createRegistrationForm();
+  }
+
+  createRegistrationForm(){
     this.validateForm = this.fb.group({
-      projectName: [null, [Validators.required]],
-      clientNAme: [null, [Validators.required]],
+      projectName: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(20)]],
+      clientName: ['Excellerent Solutions', [Validators.required]],
       projectType: [null, [Validators.required]],
       status: [null, [Validators.required]],
       supervisor: [null, [Validators.required]],
@@ -39,8 +49,16 @@ export class ProjectDetailComponent implements OnInit {
 
     });
   }
-  @ViewChild('endDatePicker') endDatePicker!: NzDatePickerComponent;
-  @ViewChild('startDatePicker') startDatepicker!:NzDatePickerComponent;
+
+  onSubmit(){
+    console.log(this.validateForm.value);
+    this.userSubmitted = true;
+  }
+
+  onReset(){
+    this.userSubmitted = false;
+    this.validateForm.reset();
+  }
 
   disabledStartDate = (startValue: Date): boolean => {
     if (!startValue || !this.validateForm.controls.endValue.value) {
@@ -64,7 +82,36 @@ export class ProjectDetailComponent implements OnInit {
   }
 
   handleEndOpenChange(open: boolean): void {
-  if(this.validateForm.controls.startValue.value)
-     this.startDatepicker.close()
-   }
+
+    console.log('handleEndOpenChange', open);
+  }
+
+  get projectName() {
+    return this.validateForm.controls.projectName as FormControl;
+  }
+
+  get clientName() {
+    return this.validateForm.controls.clientName as FormControl;
+  }
+
+  get projectType() {
+    return this.validateForm.controls.projectType as FormControl;
+  }
+
+  get status() {
+    return this.validateForm.controls.status as FormControl;
+  }
+
+  get supervisor() {
+    return this.validateForm.controls.supervisor as FormControl;
+  }
+
+  get startValue() {
+    return this.validateForm.controls.startValue as FormControl;
+  }
+
+  get endValue() {
+    return this.validateForm.controls.endValue as FormControl;
+  }
+
 }
