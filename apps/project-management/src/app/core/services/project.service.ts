a4e874cf535d5c08211fcf4a8df8ef7f46879d32
Isyas Tadese <isayasone@outllook.com>
2021-11-11 14:15:37 +0300

    project save

--- apps/project-management/src/app/core/services/project.service.ts
+++ apps/project-management/src/app/core/services/project.service.ts
@@ -1,18 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-import { Project } from '../models';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import {  tap } from 'rxjs/operators';
+import { Project, ProjectCreate } from '../models';
 import { ApiService } from '../models/apiService';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService extends ApiService<Project> {
-
-
-
-
-  constructor(protected httpClient: HttpClient ) { 
+  constructor(protected httpClient: HttpClient,private  notification: NzNotificationService ) { 
     super(httpClient);
   }
 
@@ -21,5 +18,13 @@ export class ProjectService extends ApiService<Project> {
     return 'projects';
   }
 
-
+  createProject(data:ProjectCreate)
+   {
+     this.post(data).pipe(
+           tap(()=>this.notification.success('Project Saved','')      
+            ,(error:any)=>{
+              this.notification.error('Project Not Saved','Please Try again letter');
+            }
+           ))
+  }
 }
