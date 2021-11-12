c6d3a051ad57fa60533b4c95c7fe4d1b7dd011ef
hbelew@excellerentsolutions.com <hbelew@excellerentsolutions.com>
2021-11-12 20:54:33 +0300

    added education component

--- apps/applicant-tracking/src/app/components/application/education/education.component.ts
+++ apps/applicant-tracking/src/app/components/application/education/education.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'exec-epp-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
 })
-export class EducationComponent implements OnInit {
+export class EducationComponent {
+  loggedInUser: any;
+  isModalVisible = false;
+  
+  public education = new FormGroup({
+    inistitution: new FormControl('', [Validators.required]),
+    yearFrom: new FormControl(null, [Validators.required]),
+    yearTo: new FormControl(null, [Validators.required]),
+    country: new FormControl('', [Validators.required]),
+    program: new FormControl('', [Validators.required]),
+    fieldOfStudy: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  closeModal(){
+    this.isModalVisible = false;
   }
-
+  openModal(){
+    this.isModalVisible = true;
+  }
 }
