15363af3c6795bf4d3223eb0d4ac358c255b8152
fikremariam <fmoges@excellerentsolutions.com>
2021-11-17 17:49:47 +0300

    education persistance from client side

--- apps/applicant-tracking/src/app/components/application/education/education.component.ts
+++ apps/applicant-tracking/src/app/components/application/education/education.component.ts
@@ -1,5 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { EducationProgramModel } from '../../../models/education/education-programme.model';
+import { EducationModel } from '../../../models/education/education.model';
+import { FieldOfStudyModel } from '../../../models/education/field-of-study.model';
+import { ResponseDTO } from '../../../models/ResponseDTO';
+import { EducationProgrammeService } from '../../../services/applicant/education-programme.service';
+import { EducationService } from '../../../services/applicant/education.service';
+import { FieldOfStudyService } from '../../../services/applicant/field-of-study.service';
+import {
+  NotificationType,
+  NotifierService,
+} from '../../../shared/services/notifier.service';
 @Component({
   selector: 'exec-epp-education',
   templateUrl: './education.component.html',
@@ -7,15 +19,29 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class EducationComponent implements OnInit {
   info = '';
-  loggedInUser: any;
+  loggedInUser = { Guid: '3fa85f64-5717-4562-b3fc-2c963f66afa6' };
   isModalVisible = false;
   isUpdateMode = false;
   isRecordUpdated = false;
-  educations: any = [];
+  educations: [EducationModel] | [] = [];
+  educationModel: EducationModel | null = null;
   loading = false;
   is_studying = false;
 
-  public education = new FormGroup({
+  // databinding
+  showConfirm = false;
+  guid: any;
+  fetchedEducationProgramme: [EducationProgramModel] | [] = [];
+  fetchedFieldOfStudies: [FieldOfStudyModel] | [] = [];
+  constructor(
+    private eduProgService: EducationProgrammeService,
+    private fieldOfStudyService: FieldOfStudyService,
+    private educationService: EducationService,
+    private notifier: NotifierService,
+    private modal: NzModalService
+  ) {}
+
+  education = new FormGroup({
     institution: new FormControl('', [Validators.required]),
     yearFrom: new FormControl(null, [Validators.required]),
     yearTo: new FormControl(null, [Validators.required]),
@@ -23,32 +49,56 @@ export class EducationComponent implements OnInit {
     program: new FormControl('', [Validators.required]),
     fieldOfStudy: new FormControl('', [Validators.required]),
     isStudying: new FormControl(false, [Validators.required]),
+    otherFieldOfStudy: new FormControl(null),
   });
 
   public validation = new FormGroup({
     isMultitpleEntry: new FormControl(false, [Validators.required]),
   });
-
-
-
   closeModal() {
     this.isModalVisible = false;
   }
   openModal() {
     this.isModalVisible = true;
   }
-
   onSaveRecord(): void {
     this.loading = true;
   }
-
-  onEditRecord(index: number) {
+  onEditRecord(guid: string | null) {
     this.isUpdateMode = true;
-    this.isModalVisible = true;
-    this.education.setValue({ ...this.educations[index] });
+    this.openModal();
+    const id: string = guid == null ? '' : guid;
+    this.guid = id;
+    this.educationService.getById(id).subscribe(
+      (res: ResponseDTO<[EducationModel]>) => {
+        const row = res.Data[0];
+        this.education.patchValue({
+          institution: row.Institution,
+          yearFrom: new Date(row.DateFrom),
+          yearTo: new Date(row.DateTo),
+          country: row.Country,
+          program: row.EducationProgramme?.Guid, 
+          fieldOfStudy: row.FieldOfStudy?.Guid, 
+          isStudying: row.IsCompleted,
+          otherFieldOfStudy: row.OtherFieldOfStudy,
+        });
+      },
+      (err) => console.log(err)
+    );
   }
-  onDeleteRecord(index: number) {
-    this.isUpdateMode = false;
+  onDeleteRecord(guid: string | null) {
+    this.showConfirmation(guid);
+   
+  }
+  deleteItem(guid: string | null)
+  {
+    const id = guid?guid:'';
+    this.educationService.delete(id)
+    .subscribe(_ => {
+      this.notifier.notify(NotificationType.success, 'Record deleted successfully');
+      this.bindRecord();
+  });
+
   }
 
   disabledStartDate = (startValue: Date): boolean => {
@@ -64,31 +114,81 @@ export class EducationComponent implements OnInit {
     if (!endValue || !this.education.controls.yearFrom.value) {
       return false;
     }
-    return (
-      endValue.getTime() <= this.education.controls.yearFrom.value.getTime()
-    );
+    // return (
+    //   endValue.getTime() <= this.education.controls.yearFrom.value.getTime()
+    // );
+    return false;
   };
 
   onCountryChange(value: any) {
     this.education.controls.country.setValue(value);
   }
-  addItem() {
-    this.educations = [...this.educations, this.education.value];
-    if (!this.validation.controls.isMultitpleEntry.value) {
-      this.isModalVisible = false;
-      this.info = 'Item added successfully!';
-      this.isRecordUpdated = true;
-    }
-    this.education.reset();
-    this.validation.controls.isMultitpleEntry.setValue(false);
+  async onFormSubmit() {
+    this.loading = true;
+    const educationModel = this.getFormValue();
+    if (!this.isUpdateMode) await this.addItem(educationModel);
+    else await this.updateItem(educationModel);
+    
+   
+  }
+  addItem(educationModel: EducationModel) {
+    this.educationService.add(educationModel).subscribe(
+      (_) => {
+        this.onSaveCompleted();
+        this.bindRecord();
+      },
+      (err) => console.log(err)
+    );
+  }
+  updateItem(educationModel: EducationModel) {
+    this.educationService.update(educationModel).subscribe(
+      (_) => {
+        this.onSaveCompleted();
+        this.bindRecord();
+      },
+      (err: any) => 
+      {
+        this.onShowError(err);
+      }
+    );
+  }
+  getFormValue(): EducationModel {
+    const educationModel = {
+      Guid: this.guid ? this.guid : null,
+      ApplicantId: this.loggedInUser.Guid,
+      Institution: this.education.get('institution')?.value,
+      DateFrom: new Date( this.education.get('yearFrom')?.value),
+      DateTo: new Date (this.education.get('yearTo')?.value),
+      Country: this.education.get('country')?.value,
+      EducationProgrammeId: this.education.get('program')?.value,
+      EducationProgramme: null,
+      FieldOfStudyId: this.education.get('fieldOfStudy')?.value,
+      FieldOfStudy: null,
+      OtherFieldOfStudy: this.education.get('otherFieldOfStudy')?.value,
+      IsCompleted: this.education.get('isStudying')?.value,
+    };
+    return educationModel;
   }
 
-  onFormSubmit() {
-    this.onSaveRecord();
-    this.isModalVisible = false;
+  onSaveCompleted() {
+    if (!this.validation.controls.isMultitpleEntry.value) {
+      this.closeModal();
+      this.isRecordUpdated = true;
+    }
+    this.loading = false;
+    this.notifier.notify(
+      NotificationType.success,
+      'Record is successfully added.'
+    );
+    this.education.reset();
+    this.guid = null;
+    this.loading = false;
+    this.closeModal();
+    this.isUpdateMode = false;
   }
 
   ngOnInit(): void {
+    this.bindRecord();
     this.education.controls.isStudying.valueChanges.subscribe((value) => {
       if (value) {
         this.education.controls.yearTo.setValidators([]);
@@ -97,5 +197,43 @@ export class EducationComponent implements OnInit {
         this.education.controls.yearTo.setValidators([Validators.required]);
       this.education.controls.yearTo.updateValueAndValidity();
     });
+
+    //api-integration
+    this.eduProgService.get().subscribe(
+      (res) => {
+        this.fetchedEducationProgramme = res;
+      },
+      (err) => {console.log(err)}
+    );
+    this.fieldOfStudyService.get().subscribe(
+      (res: ResponseDTO<[FieldOfStudyModel]>) => {
+        this.fetchedFieldOfStudies = res.Data;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
+  bindRecord() {
+    this.educationService.getByApplicantId(this.loggedInUser.Guid).subscribe(
+      (res: ResponseDTO<[EducationModel]>) => {
+        this.educations = res.Data;
+      },
+      (err: any) => console.log(err)
+    );
+  }
+  showConfirmation(guid: string | null): void {
+    this.modal.confirm({
+      nzTitle: 'Confirm',
+      nzContent: 'Do you want to delete this record?',
+      nzOnOk: () => { this.deleteItem(guid) }
+       
+    });
 }
+onShowError(err: any) {
+  this.notifier.notify(NotificationType.error, 'Some error occured. Please review your input and try again.');
+  this.loading = false;
+}
+}
+
+
---
+++ apps/applicant-tracking/src/app/shared/services/error-handle.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'apps/applicant-tracking/src/environments/environment';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrHandleService {
+
+    formatErrors(error:any) {
+    // this.log('education programme error:');
+    // this.log(error);
+    return throwError(error.error);
+  }
+
+  log(data: any) {
+    console.log(data);
+  }
+}
