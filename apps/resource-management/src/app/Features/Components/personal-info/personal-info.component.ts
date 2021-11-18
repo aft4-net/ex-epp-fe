73b5c4b343db272a6f87d94293724a1125bf462e
Nathan Daniel <46191474+zewdnehnathan@users.noreply.github.com>
2021-11-18 18:13:51 +0300

    personal detail layout laid

--- apps/resource-management/src/app/Features/Components/personal-info/personal-info.component.ts
+++ apps/resource-management/src/app/Features/Components/personal-info/personal-info.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
@@ -9,9 +12,26 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 })
 export class PersonalInfoComponent implements OnInit {
 
-  constructor() { }
+  validateForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      personalEmail: [null, [Validators.email, Validators.required]],
+      firstName: [null, [Validators.required]],
+      fatherName: [null, [Validators.required]],
+      grandFatherName: [null, [Validators.required]],
+      phoneNumber: [null,[Validators.required]],
+      dateofBirth: [null, [Validators.required]],
+      gender: [null, [Validators.required]],
+      nationality: [null, [Validators.required]],
+      phoneNumberPrefix:[null,[Validators.required]]
+
+    });
   }
+
+
+
 
 }
