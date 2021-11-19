7249707b5253c112154ab3f4fe7c46f4bfc8f97e
Nathan Daniel <46191474+zewdnehnathan@users.noreply.github.com>
2021-11-19 17:25:59 +0300

    dumy post and state management framework added

--- apps/resource-management/src/app/Features/Components/personal-info/personal-info.component.ts
+++ apps/resource-management/src/app/Features/Components/personal-info/personal-info.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { Employee } from '../../Models/Employee';
+import { EmployeeService } from '../../Services/Employee/EmployeeService';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { ValueTransformer } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'exec-epp-personal-info',
@@ -14,10 +17,10 @@ export class PersonalInfoComponent implements OnInit {
 
   validateForm!: FormGroup;
   listOfOption: string[] = ["male","female"];
-  listOfOptionNationality: string[] = ["Ethiopia","US","Kenya","UK","China"]
+  listOfOptionNationality: string[] = ["Ethiopia","US","Kenya","UK","China"];
+  employee !: Employee;
 
-
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,private employeeService:EmployeeService) { }
 
   ngOnInit(): void {
     this.validateForm = this.fb.group({
@@ -32,6 +35,33 @@ export class PersonalInfoComponent implements OnInit {
       phoneNumberPrefix:[null,[Validators.required]]
 
     });
+    
+    this.employee = {
+      FirstName: "Nathan",
+      FatherName: "Daniel",
+      GrandFatherName: "Zewdneh",
+      MobilePhone: "+25112345677",
+      Phone1: "25112345673",
+      Phone2: "25112345673",
+      PersonalEmail: "Abel@gmail.com",
+      PersonalEmail2: "selam@gmail.com",
+      PersonalEmail3: "nathan@gmail.com",
+      DateofBirth : new Date("2021-11-17 14:29:03.107"),
+      Gender : "Male",
+     /* Nationality: Nationality[],
+      Organization: EmployeeOrganization,
+      PersonalAddress: Address[],
+      FamilyDetail: FamilyDetails[],
+      EmergencyContact: EmergencyContact[]*/
+    }
+    
+   
+    
+
+  }
+  addEmployee(){
+    console.log("Adde Employee Executed");
+    this.employeeService.addEmployee(this.employee);
   }
 
 
---
+++ apps/resource-management/src/app/Features/Models/Employee.ts
@@ -0,0 +1,26 @@
+import { Address } from "./address.model";
+import { EmergencyContact } from "./EmergencyContactEntity";
+import { EmployeeOrganization } from "./EmployeeOrganization/EmployeeOrganization";
+import { FamilyDetails } from "./FamilyDetails";
+import { Nationality } from "./Nationality";
+
+export interface Employee{
+
+  FirstName: string,
+  FatherName: string,
+  GrandFatherName: string,
+  MobilePhone: string,
+  Phone1: string,
+  Phone2: string,
+  PersonalEmail: string,
+  PersonalEmail2: string,
+  PersonalEmail3: string,
+  DateofBirth : Date,
+  Gender : string,
+  Nationality?: Nationality[],
+  Organization?: EmployeeOrganization,
+  PersonalAddress?: Address[],
+  FamilyDetail?: FamilyDetails[],
+  EmergencyContact?: EmergencyContact[]
+  
+}
