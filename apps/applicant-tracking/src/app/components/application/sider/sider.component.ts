ac6359a9f168da512f34f545aeb1b15657e8c4cb
Abrham-Yifter <88376753+Abrham-Yifter@users.noreply.github.com>
2021-11-19 14:19:35 +0300

    added fixes to comments on applicant ui

--- apps/applicant-tracking/src/app/components/application/sider/sider.component.ts
+++ apps/applicant-tracking/src/app/components/application/sider/sider.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd, provideRoutes } from '@angular/router';
+import { ApplicantGeneralInfoService } from '../../../services/applicant/applicant-general-info.service';
+import { AreasOfInterestService } from '../../../services/applicant/areas-of-interest.service';
 import { AccountService } from '../../../services/user/account.service';
 
 interface RouteLinks {
@@ -13,36 +15,53 @@ interface RouteLinks {
   styleUrls: ['./sider.component.css'],
 })
 export class SiderComponent implements OnInit {
-  route: string = '';
+  route = '';
 
-  generalSubmitCheck: boolean = false;
-  aofSubmitCheck: boolean = false;
-  educationSubmitCheck: boolean = false;
-  wExperienceSubmitCheck: boolean = false;
-  linkSubmitCheck: boolean = false;
-  addDocumentSubmitCheck: boolean = false;
+  generalSubmitCheck = false;
+  aofSubmitCheck = false;
+  educationSubmitCheck = false;
+  wExperienceSubmitCheck = false;
+  linkSubmitCheck = false;
+  addDocumentSubmitCheck = false;
 
-  // generalInfo = {} as PersonalInformation[];
-  // profile = <PersonalInformation>{}
   checker1: any = false;
   counter: any = 1;
+
+  user = this.accountService.userInfo;
+
   activePath(routePath: string) {
     if (this.route === '') this.route = this.router.url;
     return this.route == routePath;
   }
 
   personalCheck() {
-    // this.accountService.generalInfo(32).subscribe(res => {
-    //   this.generalInfo = res.data;
-    //   this.profile = this.department[0];
-    //   if (this.dept.departmentName) {this.generalSubmitCheck = true;}
-    //   console.log(this.dept);
-    // }, error =>{
-    //   console.log(error);
-    // });
+    this.generalInfoService.getPersonalInfo({ email: this.user.Email })
+    .subscribe((response) => {
+      if(response.Data.ContactNumber){
+        this.generalSubmitCheck = true;
+      }
+    });
   }
 
-  constructor(private router: Router, private accountService: AccountService) {
+  aoiCheck() {
+    this.aoiService.data.subscribe((response) => {
+      this.aofSubmitCheck = response;
+    });
+  }
+
+  educationCheck() {
+    this.generalInfoService.getPersonalInfo({ email: this.user.Email })
+    .subscribe((response) => {
+      if(response.Data.ContactNumber){
+        this.generalSubmitCheck = true;
+      }
+    });
+  }
+
+  constructor(private router: Router, 
+    private accountService: AccountService, 
+    private aoiService: AreasOfInterestService, 
+    private generalInfoService: ApplicantGeneralInfoService) {
     router.events.subscribe((evt: any) => {
       if (evt instanceof NavigationEnd) this.route = evt.url;
     });
@@ -50,5 +69,6 @@ export class SiderComponent implements OnInit {
 
   ngOnInit(): void {
     this.personalCheck();
+    this.aoiCheck();
   }
 }
