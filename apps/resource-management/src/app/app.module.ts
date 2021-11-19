57af13115f15ac357b4d0d6d25f3670e950b6930
simbo <simboo100@gmail.com>
2021-11-19 05:00:25 -0800

    fixed

--- apps/resource-management/src/app/app.module.ts
+++ apps/resource-management/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { AddEmergencycontactComponent } from './Features/Components/emergencycontact/add-emergencycontact/add-emergencycontact.component';
 import { AddressNewComponent } from './Features/Components/address-new/address-new.component';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,15 +11,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { NgModule } from '@angular/core';
+import { OrganizationDetailComponent } from './Features/Components/organization-detail/organization-detail.component';
+import { PageBreadcrumbComponent } from './components/page-breadcrumb/page-breadcrumb.component';
 import { PageFooterComponent } from './components/page-footer/page-footer.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { PageTitleComponent } from './components/page-title/page-title.component';
-
-import { PageBreadcrumbComponent } from './components/page-breadcrumb/page-breadcrumb.component';
-import { OrganizationDetailComponent } from './Features/Components/organization-detail/organization-detail.component';
-import { ProgressButtonsComponent } from './components/progress-buttons/progress-buttons.component';
-
 import { PersonalInfoComponent } from './Features/Components/personal-info/personal-info.component';
+import { ProgressButtonsComponent } from './components/progress-buttons/progress-buttons.component';
 import en from '@angular/common/locales/en';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
@@ -28,10 +27,16 @@ registerLocaleData(en);
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent, PageHeaderComponent, PageFooterComponent, PageTitleComponent, PageBreadcrumbComponent, OrganizationDetailComponent,
+    MainLayoutComponent,
+    PageHeaderComponent,
+    PageFooterComponent,
+    PageTitleComponent,
+    PageBreadcrumbComponent,
+    OrganizationDetailComponent,
     AddressNewComponent,
     ProgressButtonsComponent,
-    PersonalInfoComponent
+    PersonalInfoComponent,
+    AddEmergencycontactComponent,
   ],
   imports: [
     BrowserModule,
@@ -40,7 +45,7 @@ registerLocaleData(en);
     HttpClientModule,
     BrowserAnimationsModule,
     DemoNgZorroAntdModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
