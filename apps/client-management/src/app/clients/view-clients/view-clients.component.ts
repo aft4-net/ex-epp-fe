d658fab07da9c4e7bbd13f6be7ddcfd651f59478
AmanuelZewdu <azewdu87@gmail.com>
2021-11-23 21:50:14 +0300

    ClientView Layout

--- apps/client-management/src/app/clients/view-clients/view-clients.component.ts
+++ apps/client-management/src/app/clients/view-clients/view-clients.component.ts
@@ -1,5 +1,8 @@
+import { Address } from './../../../../../resource-management/src/app/Features/Models/address.model';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
 
 @Component({
   selector: 'exec-epp-view-clients',
@@ -7,15 +10,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./view-clients.component.scss']
 })
 export class ViewClientsComponent implements OnInit  {
-
+   values = [
+    {client:'CocaCola',address:'Ethiopia',status:'Active',sales_person:'Yonas',client_contact:'Ayalew',company_contact:'Seifu'},
+    {client:'CocaCola',address:'USA',status:'Active',sales_person:'Abebe',client_contact:'Jhon',company_contact:'Jimy'},
+    {client:'CocaCola',address:'USA',status:'Active',sales_person:'Zerihun',client_contact:'Henock',company_contact:'Haile'},
+    {client:'CocaCola',address:'Canada',status:'Active',sales_person:'Robel',client_contact:'Yonatan',company_contact:'Pete'},
+  ]
+  
   constructor(private router:Router) { }
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
   }
-
+  
   addClientPage()
   {
      this.router.navigateByUrl('clients/add-client');
   }
-
+  
 }
