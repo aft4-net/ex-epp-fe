1d7afa464820da05c96efd1e6e6b902ed1516028
Isayas Tadesse <ITadesse@excellerentsolutions.com>
2021-11-24 23:44:26 +0300

    Add Client service And Model

--- apps/client-management/src/app/core/services/client.service.ts
+++ apps/client-management/src/app/core/services/client.service.ts
@@ -6,13 +6,15 @@ import { BehaviorSubject } from 'rxjs';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { Router } from '@angular/router';
 
-
 @Injectable({
   providedIn: 'root',
 })
 export class ClientService extends ApiService<Client> {
-  constructor(protected httpClient: HttpClient,private notification: NzNotificationService,
-    private router: Router) {
+  constructor(
+    protected httpClient: HttpClient,
+    private notification: NzNotificationService,
+    private router: Router
+  ) {
     super(httpClient);
   }
   private createClientSource = new BehaviorSubject<CreateClient>(
@@ -31,21 +33,28 @@ export class ClientService extends ApiService<Client> {
   getCreateClientDataValue() {
     return this.createClientSource.value;
   }
-  
-  addClient()
-  {
-    if(this.getCreateClientDataValue()!={} as CreateClient)
-    {
+
+  addClient() {
+    if (this.getCreateClientDataValue() != ({} as CreateClient)) {
       this.post(this.getCreateClientDataValue()).subscribe(
-        () => {
-          this.notification.success('Client Added Successfully', ''); 
+        (response: any) => {
+          if (response.ResponseStatus == 'error')
+            this.notification.error(
+              'Client Not Added',
+              'Please try again letter'
+            );
+          else {
+            this.notification.success('Client Added Successfully', '');
+            this.updateCreateClientData({} as CreateClient);
+          }
         },
         () => {
-          this.notification.error('Client Not Data Not Saved', 'Please try again letter');
+          this.notification.error(
+            'Client  Not Added',
+            'Please try again letter'
+          );
         }
       );
     }
-  
-  } 
-
+  }
 }
