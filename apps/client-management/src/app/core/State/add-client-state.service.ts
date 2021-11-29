b574ee6de8f196d17c294fd72e017ed3b3abe375
Isayas Tadesse <ITadesse@excellerentsolutions.com>
2021-11-29 20:38:35 +0300

    defulat pagination

--- apps/client-management/src/app/core/State/add-client-state.service.ts
+++ apps/client-management/src/app/core/State/add-client-state.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import {
-
   BillingAddressCreate,
   ClientContactCreate,
   ClientCreate,
