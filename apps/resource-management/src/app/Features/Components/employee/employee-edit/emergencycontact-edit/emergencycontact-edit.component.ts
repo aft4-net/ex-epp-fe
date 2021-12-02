a50f8a31908dc2e53cae9a5fd5e410f5ffa3dc87
simbo <simboo100@gmail.com>
2021-12-02 09:46:39 +0300

    edit employee components

---
+++ apps/resource-management/src/app/Features/Components/employee/employee-edit/emergencycontact-edit/emergencycontact-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exec-epp-emergencycontact-edit',
+  templateUrl: './emergencycontact-edit.component.html',
+  styleUrls: ['./emergencycontact-edit.component.scss']
+})
+export class EmergencycontactEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
