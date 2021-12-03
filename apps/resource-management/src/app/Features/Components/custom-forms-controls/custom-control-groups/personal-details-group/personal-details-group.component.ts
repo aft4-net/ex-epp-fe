38c397c985cc5501c3c51fe9e78794f477609ab2
Ashenafi-Fiseha <afiseha@excellerentsolutions.com>
2021-12-03 16:40:12 +0300

    on personal details

--- apps/resource-management/src/app/Features/Components/custom-forms-controls/custom-control-groups/personal-details-group/personal-details-group.component.ts
+++ apps/resource-management/src/app/Features/Components/custom-forms-controls/custom-control-groups/personal-details-group/personal-details-group.component.ts
@@ -19,30 +19,17 @@ export class PersonalDetailGroupComponent implements OnInit {
         private readonly _formBuilder: FormBuilder,
         private readonly _formGenerator: FormGenerator
     ) {
-        this.formGroup = this._formGenerator.getPersonalDetailsForm()
+        this.formGroup = this.personalDetailGroup? this.personalDetailGroup:this._formGenerator.getPersonalDetailsForm()
+        
     }
 
     ngOnInit(): void {
     }
 
+
+
     getControl(name: string): FormControl {
         return this.formGroup.get(name) as FormControl
-    }
-
-    createBasicForm() {
-        const form = this._formBuilder.group({
-            employeeIdNumberPrefix: [null],
-            employeeIdNumber: [null],
-            firstName: [null],
-            middleName: [null],
-            lastName: [null],
-            gender: [null],
-            dateofBirth: [null],
-            phoneNumbers: this._formBuilder.array([]),
-            EmailAddresses: this._formBuilder.array([]),
-            nationalities: [null]
-        });
-        return form
     }
 
     showData() {
