5385943aa702a3996444fdd50d529fc778f214ac
simbo <simboo100@gmail.com>
2021-12-06 20:14:27 +0300

    emergencycontact

--- apps/resource-management/src/app/Features/Components/employee/family-detail-view/family-detail-view.component.ts
+++ apps/resource-management/src/app/Features/Components/employee/family-detail-view/family-detail-view.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Data } from '@angular/router';
 import { FamilyDetailComponent } from '../family-detail/family-detail.component';
 import { NzModalService } from 'ng-zorro-antd/modal';
+import { FormGenerator } from '../../custom-forms-controls/form-generator.model';
+import { FamilyDetail } from '../../../Models/FamilyDetail/FamilyDetailModel';
 
 @Component({
   selector: 'exec-epp-family-detail-view',
@@ -16,14 +18,11 @@ export class FamilyDetailViewComponent implements OnInit {
   checked = false;
   loading = false;
   indeterminate = false;
-  listOfData: readonly Data[] = [];
-  listOfCurrentPageData: readonly Data[] = [];
-  setOfCheckedId = new Set<number>();
-
-  i = 0;
+  listOfFamilies: readonly FamilyDetail[] = [];
+  
   editId: string | null = null;
 
-  constructor(private modalService: NzModalService) {}
+  constructor(private modalService: NzModalService, public form: FormGenerator) {}
 
   addfamilies(): void {
     this.isVisible = true;
@@ -42,57 +41,10 @@ export class FamilyDetailViewComponent implements OnInit {
     }, 3000);
   }
 
-  handleCancel(): void {
-    this.isVisible = false;
-  }
+  
+ 
 
-  updateCheckedSet(id: number, checked: boolean): void {
-    if (checked) {
-      this.setOfCheckedId.add(id);
-    } else {
-      this.setOfCheckedId.delete(id);
-    }
-  }
-
-  onCurrentPageDataChange(listOfCurrentPageData: readonly Data[]): void {
-    this.listOfCurrentPageData = listOfCurrentPageData;
-    this.refreshCheckedStatus();
-  }
-
-  refreshCheckedStatus(): void {
-    const listOfEnabledData = this.listOfCurrentPageData.filter(
-      ({ disabled }) => !disabled
-    );
-    this.checked = listOfEnabledData.every(({ id }) =>
-      this.setOfCheckedId.has(id)
-    );
-    this.indeterminate =
-      listOfEnabledData.some(({ id }) => this.setOfCheckedId.has(id)) &&
-      !this.checked;
-  }
-
-  onItemChecked(id: number, checked: boolean): void {
-    this.updateCheckedSet(id, checked);
-    this.refreshCheckedStatus();
-  }
-
-  onAllChecked(checked: boolean): void {
-    this.listOfCurrentPageData
-      .filter(({ disabled }) => !disabled)
-      .forEach(({ id }) => this.updateCheckedSet(id, checked));
-    this.refreshCheckedStatus();
-  }
-
-  ngOnInit(): void {
-    this.listOfData = new Array(1).fill(0).map((_, index) => ({
-      id: index,
-      name: `Edward King `,
-      age: 32,
-      address: `London, Park Lane no. ${index}`,
-      disabled: index % 2 === 0,
-    }));
-  }
-
+ 
   startEdit(id: string): void {
     this.editId = id;
     this.isVisible = true;
@@ -102,31 +54,14 @@ export class FamilyDetailViewComponent implements OnInit {
     this.editId = null;
   }
 
-  addRow(): void {
-    this.listOfData = [
-      ...this.listOfData,
-      {
-        id: `${this.i}`,
-        name: `Edward King `,
-        age: '32',
-        address: `London, Park Lane no. ${this.i}`,
-      },
-    ];
-    this.i++;
-  }
+  
 
-  deleteRow(id: string): void {
-    this.listOfData = this.listOfData.filter((d) => d.id !== id);
-  }
 
-  showModal(): void {
-    this.isVisible = true;
-  }
-
-  showConfirm(): void {
+  showConfirm(guid: string): void {
+    this.listOfFamilies = this.listOfFamilies.filter((d) => d.Guid !== guid);
     this.modalService.confirm({
       nzTitle: 'Confirm',
-      nzContent: 'Bla bla ...',
+      nzContent: 'Are you sure you want to delete?',
       nzOkText: 'OK',
       nzCancelText: 'Cancel',
     });
@@ -135,4 +70,24 @@ export class FamilyDetailViewComponent implements OnInit {
   exitModal() {
     this.isVisible = false;
   }
+
+
+  resetForm(): void {
+    this.form.familyDetail.reset();
+  }
+
+  add(): void {
+    this.isConfirmLoading = true;
+    setTimeout(() => {
+      this.isVisible = false;
+      this.isConfirmLoading = false;
+    }, 6000);
+  }
+
+
+
+  ngOnInit(): void {
+   
+  }
+
 }
