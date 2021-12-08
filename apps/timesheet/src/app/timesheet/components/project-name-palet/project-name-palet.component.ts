cc6e308ba2e750cc22c64c8d9023b10d65bfcf38
Berihun Hadis <bhadis@excellerentsolutions.com>
2021-12-08 15:35:04 +0300

    merge conflict updated

--- apps/timesheet/src/app/timesheet/components/project-name-palet/project-name-palet.component.ts
+++ apps/timesheet/src/app/timesheet/components/project-name-palet/project-name-palet.component.ts
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
-import {TimeEntryEvent} from '../../../models/clickEventEmitObjectType';
-import {ClickEventType} from '../../../models/clickEventType';
-import {Project} from '../../../models/project';
-import {TimeEntry, TimesheetApproval} from '../../../models/timesheetModels';
-import {TimesheetService} from '../../services/timesheet.service';
-import {NzModalService} from "ng-zorro-antd/modal";
-=======
+
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TimeEntryEvent } from '../../../models/clickEventEmitObjectType';
 import { ClickEventType } from '../../../models/clickEventType';
 import { Project } from '../../../models/project';
 import { ApprovalStatus, TimeEntry, TimesheetApproval } from '../../../models/timesheetModels';
 import { TimesheetService } from '../../services/timesheet.service';
->>>>>>> develop
+import {NzModalService} from "ng-zorro-antd/modal";
 
 @Component({
   selector: 'app-project-name-palet',
@@ -85,64 +77,10 @@ export class ProjectNamePaletComponent implements OnInit {
   closePopover() {
     this.popoverVisible = false;
   }
-<<<<<<< HEAD
 
-  showDeleteConfirm(): void {
-    // // @ts-ignore
-    // this.modal.confirm({
-    //
-    //
-    //   // cursor: 'pointer',
-    //   // background: '#FFFFFF',
-    //   // nzContent: '<b style="color: #262626; background: #FFFFFF;border: #00A551; height: 194px;radius: 2px;padding: 24px;">Are you sure you want to delete this entry?<br> you can\'t undo this action.</b>',
-    //   nzTitle: 'Delete?',
-    //   nzContent: '<b style="color: #262626 ">Are you sure you want to delete this entry?<br> you can not undo this action.</b>',
-    //   nzOkText: 'Yes, Delete',
-    //   nzOkType: 'primary',
-    //
-    //
-    //   // border: '#00A551',
-    //   // radius: '2px',
-    //   // top: '357px',
-    //   // width: '328px',
-    //   // padding: '24px',
-    //   // left: '532px',
-    //   nzOkDanger: true,
-    //   nzCancelText: 'Cancel',
-    //   nzOnCancel: () => console.log('Cancel'),
-    //   nzOnOk: () => console.log('OK'),
-    //
-    // });
-    this.isVisible1 = true;
-  }
   showModal(): void {
     this.isVisible1 = true;
-    this.modal.confirm({
 
-        //
-        //
-        //   // cursor: 'pointer',
-        //   // background: '#FFFFFF',
-        //   // nzContent: '<b style="color: #262626; background: #FFFFFF;border: #00A551; height: 194px;radius: 2px;padding: 24px;">Are you sure you want to delete this entry?<br> you can\'t undo this action.</b>',
-        //   nzTitle: 'Delete?',
-        //   nzContent: '<b style="color: #262626 ">Are you sure you want to delete this entry?<br> you can not undo this action.</b>',
-        //   nzOkText: 'Yes, Delete',
-        //   nzOkType: 'primary',
-        //
-        //
-        //   // border: '#00A551',
-        //   // radius: '2px',
-        //   // top: '357px',
-        //   // width: '328px',
-        //   // padding: '24px',
-        //   // left: '532px',
-        //   nzOkDanger: true,
-        //   nzCancelText: 'Cancel',
-        //   nzOnCancel: () => console.log('Cancel'),
-        //   nzOnOk: () => console.log('OK'),
-        //
-        // });
-    });
   }
 
   handleOk(): void {
@@ -154,12 +92,13 @@ export class ProjectNamePaletComponent implements OnInit {
     console.log('Button cancel clicked!');
     this.isVisible1 = false;
   }
-=======
+
   deleteTimeEntry(Guid:string):void{
+    this.isVisible1 = true;
     this.timesheetService.deleteTimeEntry(Guid).subscribe(data => {
         console.log(data);
       });
 
     }
->>>>>>> develop
+
 }
