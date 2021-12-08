972c37e204c5692e64f5bbc1b9ef8baea82dc5ae
Berihun Hadis <bhadis@excellerentsolutions.com>
2021-12-08 13:59:32 +0300

    delete popup confirmation done

--- apps/timesheet/src/app/timesheet/components/project-name-palet/project-name-palet.component.ts
+++ apps/timesheet/src/app/timesheet/components/project-name-palet/project-name-palet.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { TimeEntryEvent } from '../../../models/clickEventEmitObjectType';
-import { ClickEventType } from '../../../models/clickEventType';
-import { Project } from '../../../models/project';
-import { TimeEntry, TimesheetApproval } from '../../../models/timesheetModels';
-import { TimesheetService } from '../../services/timesheet.service';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {TimeEntryEvent} from '../../../models/clickEventEmitObjectType';
+import {ClickEventType} from '../../../models/clickEventType';
+import {Project} from '../../../models/project';
+import {TimeEntry, TimesheetApproval} from '../../../models/timesheetModels';
+import {TimesheetService} from '../../services/timesheet.service';
+import {NzModalService} from "ng-zorro-antd/modal";
 
 @Component({
   selector: 'app-project-name-palet',
@@ -19,10 +20,13 @@ export class ProjectNamePaletComponent implements OnInit {
   project: Project | null = null;
   projectNamePaletClass = "project-name-palet"
 
+  isVisible1 = false;
   clickEventType = ClickEventType.none;
   popoverVisible = false;
 
-  constructor(private timesheetService: TimesheetService) { }
+  constructor(private timesheetService: TimesheetService,
+              private modal: NzModalService) {
+  }
 
   ngOnInit(): void {
     if (this.timeEntry) {
@@ -33,8 +37,7 @@ export class ProjectNamePaletComponent implements OnInit {
 
     if (this.timesheetApproval && this.timesheetApproval.Status != 2) {
       this.projectNamePaletClass = "project-name-palet-approval";
-    }
-    else{
+    } else {
       this.projectNamePaletClass = "project-name-palet";
     }
   }
@@ -48,18 +51,18 @@ export class ProjectNamePaletComponent implements OnInit {
       this.paletEllipsisClicked.emit(timeEntryEvent);
       this.popoverVisible = this.timesheetApproval ? this.timesheetApproval.Status === 2 : true;
     }
-  } 
+  }
 
   onProjectNamePaletClicked() {
     let timeEntryEvent: TimeEntryEvent = {clickEventType: ClickEventType.showFormDrawer, timeEntry: this.timeEntry};
 
-    if (this.clickEventType == ClickEventType.none){
+    if (this.clickEventType == ClickEventType.none) {
       this.clickEventType = ClickEventType.showFormDrawer;
       this.projectNamePaletClicked.emit(timeEntryEvent);
     }
 
     this.clickEventType = ClickEventType.none; //Use this line of code when the element is the container element.
-  }  
+  }
 
   showFormDrawer() {
     if (this.clickEventType === ClickEventType.none) {
@@ -72,5 +75,73 @@ export class ProjectNamePaletComponent implements OnInit {
 
   closePopover() {
     this.popoverVisible = false;
+  }
+
+  showDeleteConfirm(): void {
+    // // @ts-ignore
+    // this.modal.confirm({
+    //
+    //
+    //   // cursor: 'pointer',
+    //   // background: '#FFFFFF',
+    //   // nzContent: '<b style="color: #262626; background: #FFFFFF;border: #00A551; height: 194px;radius: 2px;padding: 24px;">Are you sure you want to delete this entry?<br> you can\'t undo this action.</b>',
+    //   nzTitle: 'Delete?',
+    //   nzContent: '<b style="color: #262626 ">Are you sure you want to delete this entry?<br> you can not undo this action.</b>',
+    //   nzOkText: 'Yes, Delete',
+    //   nzOkType: 'primary',
+    //
+    //
+    //   // border: '#00A551',
+    //   // radius: '2px',
+    //   // top: '357px',
+    //   // width: '328px',
+    //   // padding: '24px',
+    //   // left: '532px',
+    //   nzOkDanger: true,
+    //   nzCancelText: 'Cancel',
+    //   nzOnCancel: () => console.log('Cancel'),
+    //   nzOnOk: () => console.log('OK'),
+    //
+    // });
+    this.isVisible1 = true;
+  }
+  showModal(): void {
+    this.isVisible1 = true;
+    this.modal.confirm({
+
+        //
+        //
+        //   // cursor: 'pointer',
+        //   // background: '#FFFFFF',
+        //   // nzContent: '<b style="color: #262626; background: #FFFFFF;border: #00A551; height: 194px;radius: 2px;padding: 24px;">Are you sure you want to delete this entry?<br> you can\'t undo this action.</b>',
+        //   nzTitle: 'Delete?',
+        //   nzContent: '<b style="color: #262626 ">Are you sure you want to delete this entry?<br> you can not undo this action.</b>',
+        //   nzOkText: 'Yes, Delete',
+        //   nzOkType: 'primary',
+        //
+        //
+        //   // border: '#00A551',
+        //   // radius: '2px',
+        //   // top: '357px',
+        //   // width: '328px',
+        //   // padding: '24px',
+        //   // left: '532px',
+        //   nzOkDanger: true,
+        //   nzCancelText: 'Cancel',
+        //   nzOnCancel: () => console.log('Cancel'),
+        //   nzOnOk: () => console.log('OK'),
+        //
+        // });
+    });
+  }
+
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible1 = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible1 = false;
   }
 }
