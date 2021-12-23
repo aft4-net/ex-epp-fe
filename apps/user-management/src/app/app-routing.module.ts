ae414bc548370d209ebd4e4df32bb6e36514fe96
Nathan Daniel <46191474+zewdnehnathan@users.noreply.github.com>
2021-12-23 20:55:29 +0300

    router module

---
+++ apps/user-management/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { EppdashboardComponent } from './features/components/eppdashboard/eppdashboard.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  { path: '', component: EppdashboardComponent },
+
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
--- apps/user-management/src/app/app.module.ts
+++ apps/user-management/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { EppdashboardComponent } from './features/components/eppdashboard/eppdashboard.component';
+import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, EppdashboardComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent],
