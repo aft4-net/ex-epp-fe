ebf24490325d361e59f3d463154a659b68e562ac
yosef kefale <yoseph.kefale@gmail.com>
2021-12-23 14:24:46 +0300

    timesheet-approval module created

---
+++ apps/timesheet/src/app/timesheet-approval/timesheet-approval.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exec-epp-timesheet-approval',
+  templateUrl: './timesheet-approval.component.html',
+  styleUrls: ['./timesheet-approval.component.css']
+})
+export class TimesheetApprovalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
