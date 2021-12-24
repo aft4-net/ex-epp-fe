1596d502a9c7a1d23f6417eeb2f39933c2177f5f
isayasone <60773892+isayasone@users.noreply.github.com>
2021-12-24 15:29:42 +0300

    intial

---
+++ apps/timesheet/src/app/timesheet/components/view-submissions/view-submissions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exec-epp-view-submissions',
+  templateUrl: './view-submissions.component.html',
+  styleUrls: ['./view-submissions.component.scss']
+})
+export class ViewSubmissionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
