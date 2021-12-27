fd5bdf92a434205010e9f443210e7d31403b7ecc
fikremariam <fmoges@excellerentsolutions.com>
2021-12-27 17:22:18 +0300

    adding componenets

--- apps/user-management/src/app/app-routing.module.ts
+++ apps/user-management/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { CommonModule } from '@angular/common';
 import { EppdashboardComponent } from './features/components/eppdashboard/eppdashboard.component';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { SiderComponent } from './components/application/sider/sider.component';
+import { AddUserComponent } from './features/components/user/add-user/add-user.component';
 
 const routes: Routes = [
   { path: '', component: SiderComponent, },
+  {path: 'add-user', component: AddUserComponent}
 ];
 
 @NgModule({
