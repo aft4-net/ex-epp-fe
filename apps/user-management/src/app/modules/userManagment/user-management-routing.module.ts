25cde41a29cecc4daf7768bd731028929cd6b8e6
Abrham-Yifter <88376753+Abrham-Yifter@users.noreply.github.com>
2021-12-27 20:39:35 +0300

    create ui group

--- apps/user-management/src/app/modules/userManagment/user-management-routing.module.ts
+++ apps/user-management/src/app/modules/userManagment/user-management-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
       {
         path: '',
         component: GroupsetComponent
-      },
+      }, 
       {
         path: 'groups',
         component: GroupsetComponent
