1316d5c11915119acfb12d19b8fda1c524e9378f
yosef kefale <yoseph.kefale@gmail.com>
2021-12-30 15:26:26 +0300

    table code inserted into tabs

--- apps/timesheet/src/app/timesheet-approval/timesheet-approval.component.ts
+++ apps/timesheet/src/app/timesheet-approval/timesheet-approval.component.ts
@@ -1,7 +1,20 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { PaginatedResult } from '../models/PaginatedResult';
+import { TimesheetApproval } from '../models/timesheetModels';
 import { TimesheetService } from '../timesheet/services/timesheet.service';
+
+interface ItemData {
+  id: number,
+  name: string;
+  dateRange: string;
+  projectName: number;
+  clientName: string;
+  hours: number,
+  stats: string
+}
 
 
 @Component({
@@ -15,8 +28,63 @@ export class TimesheetApprovalComponent implements OnInit {
   statusColumn = true;
   cols: TemplateRef<any>[] = [];
   currentNameSubject$ = new BehaviorSubject(true);
+  searchProject = new FormControl();
+
+  timeSheetApproval!: TimesheetApproval[];
+  timeSheetApprovalAll!: TimesheetApproval[];
+  totalAll=10;
+  pageIndexAll = 1;
+  pageSizeAll = 10;
+  totalPageAll!: number;
+
+  timeSheetApprovalAwaiting!: TimesheetApproval[];
+  totalAwaiting=10;
+  pageIndexAwaiting = 1;
+  pageSizeAwaiting = 10;
+  totalPageAwaiting!: number;
+
+  timeSheetApprovalApproved!: TimesheetApproval[];
+  totalApproved=10;
+  pageIndexApproved = 1;
+  pageSizeApproved = 10;
+  totalPageApproved!: number;
+
+  timeSheetApprovalReview!: TimesheetApproval[];
+  totalReview=10;
+  pageIndexReview = 1;
+  pageSizeReview = 10;
+  totalPageReview!: number;
+
+  loading = true;
+
+  idParam = '';
+  totalPage!: number;
+
+  //table
+
+  total=10;
+  pageIndex = 1;
+  pageSize = 10;
+
+  sortByParam="";
+  sortDirection = "asc";
+
+  checked = false;
+  indeterminate = false;
+  listOfCurrentPageData: readonly ItemData[] = [];
+  listOfData: readonly ItemData[] = [];
+  setOfCheckedId = new Set<number>();
 
   resources: any;
+
+  listOfSelection = [
+    {
+      text: 'Select All Row',
+      onSelect: () => {
+        this.onAllChecked(true);
+      }
+    }
+  ];
 
   employees = [
     {
@@ -90,37 +158,100 @@ export class TimesheetApprovalComponent implements OnInit {
     clientName: 'test',
     hours: 10,
     status: 'Approved'
+  },
+  {
+    id:16,
+    name: 'Daniel',
+    dateRange: Date.now(),
+    projectName: 'Finanace Module',
+    clientName: 'Security Finance',
+    hours: 12,
+    status: 'Awaiting Approval'
+  },
+  {
+    id: 17,
+    name: 'Abel',
+    dateRange: Date.now(),
+    projectName: 'Test',
+    clientName: 'test',
+    hours: 20,
+    status: 'Approved'
+  },
+  {
+    id: 18,
+    name: 'hana',
+    dateRange: Date.now(),
+    projectName: 'test',
+    clientName: 'test',
+    hours: 10,
+    status: 'Approved'
   }
 ];
 
-headings = [
-    'Name',
-    'Date Range',
-    'Project Name',
-    'Client Name',
-    'Hours',
-];
-
-headingswithStatus = [
-  'Name',
-  'Date Range',
-  'Project Name',
-  'Client Name',
-  'Hours',
-  'Status'
-];
 constructor(
   private router: Router,
   private timeSheetService: TimesheetService
 ) {}
 
+ngOnInit(): void {
+  this.timesheetApprovalPaginationAll(1,10);
+  this.timesheetSubmissionPaginationAwaiting(1,10);
+}
+
+timesheetApprovalPaginationAll(index: number, pageSize: number) {
+  this.timeSheetService
+    .getTimesheetApprovalPagination(index, pageSize)
+    .subscribe((response: PaginatedResult<TimesheetApproval[]>) => {
+      this.timeSheetApprovalAll = response.data;
+      this.pageIndexAll = response.pagination.pageIndex;
+      this.pageSizeAll = response.pagination.pageSize;
+      this.totalAll = response.pagination.totalRecord;
+      this.totalPageAll = response.pagination.totalPage;
+    });
+}
+
+timesheetSubmissionPaginationAwaiting(index: number, pageSize: number) {
+  this.timeSheetService
+    .getTimesheetApprovalPagination(index, pageSize)
+    .subscribe((response: PaginatedResult<TimesheetApproval[]>) => {
+      this.timeSheetApprovalAwaiting = response.data;
+      this.pageIndexAwaiting = response.pagination.pageIndex;
+      this.pageSizeAwaiting = response.pagination.pageSize;
+      this.totalAwaiting = response.pagination.totalRecord;
+      this.totalPageAwaiting = response.pagination.totalPage;
+    });
+}
+
+timesheetSubmissionPaginationApproved(index: number, pageSize: number) {
+  this.timeSheetService
+    .getTimesheetApprovalPagination(index, pageSize)
+    .subscribe((response: PaginatedResult<TimesheetApproval[]>) => {
+      this.timeSheetApprovalApproved = response.data;
+      this.pageIndexApproved = response.pagination.pageIndex;
+      this.pageSizeApproved = response.pagination.pageSize;
+      this.totalApproved = response.pagination.totalRecord;
+      this.totalPageApproved = response.pagination.totalPage;
+    });
+}
+
+timesheetSubmissionPaginationReview(index: number, pageSize: number) {
+  this.timeSheetService
+    .getTimesheetApprovalPagination(index, pageSize)
+    .subscribe((response: PaginatedResult<TimesheetApproval[]>) => {
+      this.timeSheetApprovalReview = response.data;
+      this.pageIndexReview = response.pagination.pageIndex;
+      this.pageSizeReview = response.pagination.pageSize;
+      this.totalReview = response.pagination.totalRecord;
+      this.totalPageReview = response.pagination.totalPage;
+    });
+}
+
+
 getweek(result:Date):void{
   console.log('week: ');
 }
 
-ngOnInit(): void {
 
-}
 onTabSelected(tab:any) {
   console.log(tab);
    if (tab === 1){
@@ -134,6 +265,69 @@ onTabSelected(tab:any) {
 updateProjectResourseList(resources: any) {
   this.resources = resources;
   console.log(this.resources);
+}
+// for the table
+
+
+
+PageIndexChange(index: any): void {
+  this.pageIndex = index;
+  this.loading = true;
+  this.timesheetApprovalPaginationAll(index, 10);
+  this.loading = false;
+}
+
+updateCheckedSet(id: number, checked: boolean): void {
+  if (checked) {
+    this.setOfCheckedId.add(id);
+    console.log(this.setOfCheckedId);
+  } else {
+    this.setOfCheckedId.delete(id);
+    console.log(this.setOfCheckedId);
+  }
+}
+
+onItemChecked(id: number, checked: boolean): void {
+  this.updateCheckedSet(id, checked);
+  this.refreshCheckedStatus();
+}
+
+onAllChecked(value: boolean): void {
+  this.listOfCurrentPageData.forEach(item => this.updateCheckedSet(item.id, value));
+  this.refreshCheckedStatus();
+}
+
+onCurrentPageDataChange($event: readonly ItemData[]): void {
+  this.listOfCurrentPageData = $event;
+  this.refreshCheckedStatus();
+}
+
+refreshCheckedStatus(): void {
+  this.checked = this.listOfCurrentPageData.every(item => this.setOfCheckedId.has(item.id));
+  this.indeterminate = this.listOfCurrentPageData.some(item => this.setOfCheckedId.has(item.id)) && !this.checked;
+}
+showModal(id: any) {
+
+}
+
+sorter(heading:string) {
+  if (heading === 'Name'){
+    this.sortByParam = "name";
+  } else if (heading === 'Date Range'){
+    this.sortByParam = "dateRange";
+  }else if (heading === 'Project Name') {
+    this.sortByParam = "projectName";
+  } else if (heading === 'Client Name') {
+    this.sortByParam = "clientName";
+  } else {
+    this.sortByParam = "";
+  }
+
+  if (this.sortDirection === 'desc') {
+    this.sortDirection = 'asc';
+  } else {
+    this.sortDirection = 'desc';
+  }
 }
 
 }
