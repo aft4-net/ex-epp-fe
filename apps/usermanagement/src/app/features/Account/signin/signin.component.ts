fc49a8087e9ef8b2e076db5237e77c19dd653220
Yaschalew Erkihun <yaschalewerkihun6@gmail.com>
2022-01-04 07:51:22 +0400

    permission

--- apps/usermanagement/src/app/features/Account/signin/signin.component.ts
+++ apps/usermanagement/src/app/features/Account/signin/signin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MsalService } from '@azure/msal-angular';
+import { AuthenticationResult } from '@azure/msal-browser';
 ///import{MsalService} from '@azure/msal-angular';
 //import{AuthenticationResult} from '@azure/msal-browser'
 
@@ -12,41 +14,41 @@ import { Router } from '@angular/router';
 })
 export class SigninComponent implements OnInit {
 
-  //    constructor(private authService: MsalService, private router: Router) {}
+      constructor(private authService: MsalService, private router: Router) {}
   
   ngOnInit(): void {
-    ;
-  //   this.authService.instance.handleRedirectPromise().then( res => {
-  //     if (res != null && res.account != null) {
-  //       this.authService.instance.setActiveAccount(res.account)
-  //     }
-  //   })
+    
+    this.authService.instance.handleRedirectPromise().then( res => {
+      if (res != null && res.account != null) {
+        this.authService.instance.setActiveAccount(res.account)
+      }
+    })
    }
 
    isLoggedIn(): boolean {
      return true;
-  //   return this.authService.instance.getActiveAccount() != null
+    return this.authService.instance.getActiveAccount() != null
    }
 
   login() {
-    ;
-    // // this.authService.loginRedirect();
-    // this.authService.loginPopup()
-    //   .subscribe((response: AuthenticationResult) => {
-    //  const data=   this.authService.instance.setActiveAccount(response.account);
+    
+     this.authService.loginRedirect();
+    this.authService.loginPopup()
+      .subscribe((response: AuthenticationResult) => {
+     const data=   this.authService.instance.setActiveAccount(response.account);
      
-    //    console.log('data');
-    //    console.log(response.account);
-    //    console.log(this.router.navigateByUrl('usermanagement'));
-    //    this.router.navigateByUrl('usermanagement');
+       console.log('data');
+       console.log(response.account);
+       console.log(this.router.navigateByUrl('usermanagement'));
+       this.router.navigateByUrl('usermanagement');
      
-    //  //window.location.reload();
+     window.location.reload();
         
-    //   });
+      });
   }
 
   logout() {
-   // this.authService.logout();
+    this.authService.logout();
     window.location.reload();
   }
 }
