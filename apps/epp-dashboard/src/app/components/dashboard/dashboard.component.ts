c7181dbdb2c673b016feaa80fa00d2eb1e32203f
Yaschalew Erkihun <yaschalewerkihun6@gmail.com>
2022-01-04 12:56:40 +0400

    lop

--- apps/epp-dashboard/src/app/components/dashboard/dashboard.component.ts
+++ apps/epp-dashboard/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './../../../../../../libs/common-services/Authentication.service';
 
 @Component({
   selector: 'exec-epp-dashboard',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+name:any
+  constructor(private _authenticationService:AuthenticationService) { }
 
   ngOnInit(): void {
+    this.name=this._authenticationService.getUserFullName();
   }
 
 }
