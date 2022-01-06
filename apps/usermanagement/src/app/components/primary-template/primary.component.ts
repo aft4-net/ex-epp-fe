b67288e606485358b5cf0edc7f0034b37cbab400
hbelew@excellerentsolutions.com <hbelew@excellerentsolutions.com>
2022-01-06 22:51:14 +0300

    Final Merge Conflict Resolved

--- apps/usermanagement/src/app/components/primary-template/primary.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'exec-epp-primary-page-template',
-  templateUrl: './primary.component.html',
-  styleUrls: ['./primary.component.css'],
-})
-export class PrimaryComponent {}
- 
--- apps/usermanagement/src/app/features/components/user/add-user/add-user.component.spec.ts
+++ apps/usermanagement/src/app/features/components/user/add-user/add-user.component.spec.ts
@@ -1,19 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:apps/epp-dashboard/src/app/components/dashboard/dashboard.component.spec.ts
-import { DashboardComponent } from './dashboard.component';
-
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
-=======
->>>>>>> origin
 import { AddUserComponent } from './add-user.component';
 
 describe('AddUserComponent', () => {
@@ -23,24 +9,12 @@ describe('AddUserComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ AddUserComponent ]
-<<<<<<< HEAD
-=======
->>>>>>> origin:apps/usermanagement/src/app/features/components/user/add-user/add-user.component.spec.ts
->>>>>>> origin
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-<<<<<<< HEAD
     fixture = TestBed.createComponent(AddUserComponent);
-=======
-<<<<<<< HEAD:apps/epp-dashboard/src/app/components/dashboard/dashboard.component.spec.ts
-    fixture = TestBed.createComponent(DashboardComponent);
-=======
-    fixture = TestBed.createComponent(AddUserComponent);
->>>>>>> origin:apps/usermanagement/src/app/features/components/user/add-user/add-user.component.spec.ts
->>>>>>> origin
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
