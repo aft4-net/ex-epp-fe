95f817c7dbac0ea4989acb48dcaa98210ad5c5e7
adisalem <adislem@gmail.com>
2022-01-06 12:49:51 +0300

    status name

--- apps/timesheet/src/app/timesheet/services/timesheet.service.ts
+++ apps/timesheet/src/app/timesheet/services/timesheet.service.ts
@@ -33,7 +33,8 @@ export class TimesheetService {
   baseUrl = environment.apiUrl;
   timesheetId?:string;
   timesheetApp?:Timesheet;
-
+  statusChanged=false;
+  timesheetApprove!:TimesheetApproval;
   constructor(
     private notification: NzNotificationService,
     private http: HttpClient,
@@ -451,12 +452,16 @@ export class TimesheetService {
           { nzPlacement: 'bottomRight' }
 
           );
+          this.statusChanged=true;
+          this.timesheetApprove=approval;
         }
         else{
 
           this.notification.error(this.error,"",
           { nzPlacement: 'bottomRight' }
           );
+          this.statusChanged=false;
+
         }
       });
     }
