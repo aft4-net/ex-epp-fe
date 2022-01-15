ab9589fb037fb4e347a68cd2f43bb235299008ad
Netsanet <NDaniel@excellerentsolutions.com>
2022-01-15 15:41:30 +0300

    merged for real

--- apps/epp-dashboard/src/app/components/dashboard/dashboard.component.ts
+++ apps/epp-dashboard/src/app/components/dashboard/dashboard.component.ts
@@ -9,30 +9,23 @@ import { IntialdataService } from '../../services/intialdata.service';
 })
 export class DashboardComponent implements OnInit {
 
-<<<<<<< HEAD
+
   thePosition : any;
-  fullName:any
-=======
+
   fullName:any;
   date:any;
->>>>>>> epp-module-integration
   permissionList:any[]=[ ];
   modulePermission:any[]=[];
   constructor(private _intialdataService: IntialdataService,private _authenticationService:AuthenticationService,private _permissionService:PermissionService)  { 
     setTimeout(() => {
     this.fullName=_authenticationService.getUserFullName();
     const namearray=this.fullName.split(' ');
-<<<<<<< HEAD
+
     this.fullName=namearray[0];  
+    this.date = new Date();
       this.thePosition = _authenticationService.position;
     }, 500);
-  
-=======
-    this.fullName=namearray[0];
-    
-    this.date = new Date();
- 
->>>>>>> epp-module-integration
+
   }
 
   ngOnInit(): void {
