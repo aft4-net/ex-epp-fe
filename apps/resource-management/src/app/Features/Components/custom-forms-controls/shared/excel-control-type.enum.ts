160a96dd311f0576a047a4a7cf2fb52f54a1d86c
simbo <simboo100@gmail.com>
2022-01-17 11:24:53 -0800

    delete old resource-management

--- apps/resource-management/src/app/Features/Components/custom-forms-controls/shared/excel-control-type.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum ExcelControlType {
-    ExcelButton,
-    ExcelTextBox,
-    ExcelSelect,
-    ExcelMultiSelect,
-    ExcelDatePicker,
-    ExcelEmailBox,
-    ExcelPhoneNumber
-}
--- apps/resource-management/src/app/Features/Models/device-detail/deviceclassification.ts
+++
@@ -1,9 +0,0 @@
-export interface DeviceClassification {
-    Guid: string;
-    IsActive: boolean;
-    IsDeleted: boolean;
-    CreatedDate: Date;
-    CreatedbyUserGuid: string;
-    Name: string;
-    Description: string;
-}
