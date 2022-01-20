549679a2bf033bec7aaa182b6a7911d81c79f9dd
Abel-D <70660536+Abel-D@users.noreply.github.com>
2022-01-20 15:17:30 +0300

    minor fixes

--- apps/timesheet/src/app/timesheet-approval/components/timesheet-detail-view/timesheet-detail-view.component.ts
+++ apps/timesheet/src/app/timesheet-approval/components/timesheet-detail-view/timesheet-detail-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { SelectMultipleControlValueAccessor } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { PermissionService } from 'libs/common-services/permission.service';
+import { PermissionListService} from '../../../../../../../libs/common-services/permission.service';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { ApprovalStatus, TimesheetApproval } from '../../../models/timesheetModels';
 import { TimesheetService } from '../../../timesheet/services/timesheet.service';
@@ -23,7 +23,7 @@ export class TimesheetDetailViewComponent implements OnInit {
     constructor(private timesheetService:TimesheetService,
       private notification: NzNotificationService,
       private _router: Router,
-      Private _permissionService:PermissionService,) {
+      Private readonly _permissionService:PermissionListService,) {
 
     }
 
