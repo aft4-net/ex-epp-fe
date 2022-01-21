f5e72e08c0432cbda9fc73ff41e52db9d75c727f
Joseph <yosephassefa85@gmail.com>
2022-01-21 15:59:37 +0300

    persmission for the client is done

--- apps/clientmanagement/src/app/app.component.ts
+++ apps/clientmanagement/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'clientmanagement';
+
+  constructor()
+  {
+    debugger;
+    alert('client component')
+
+  }
 }
