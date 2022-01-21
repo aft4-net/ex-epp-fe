3ff88f34c0c01aa728699763366d885dbc5f09ae
Yaschalew Erkihun <yaschalewerkihun6@gmail.com>
2022-01-21 08:46:41 +0400

    update

--- apps/clientmanagement/src/app/app.component.ts
+++ apps/clientmanagement/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonDataService } from '../../../../libs/common-services/commonData.service';
 @Component({
   selector: 'exec-epp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clientmanagement';
+  constructor(private _commonData:CommonDataService){
+
+  }
+  ngOnInit(): void {
+    
+ 
+    alert("intial")
+        this._commonData.getPermission();
+      } 
+    
+    
+  
 }
