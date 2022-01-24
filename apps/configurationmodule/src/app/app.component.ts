b22e391107841f50fe68f0d1cd3169b18391ea9b
Biniyam <btechan@excellerentsolutions.com>
2022-01-24 15:38:57 +0300

    update

--- apps/configurationmodule/src/app/app.component.ts
+++ apps/configurationmodule/src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 
 @Component({
   selector: 'exec-epp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'configurationmodule';
   isLogin=true;
   route='';
-  constructor( private router: Router){
-
+  constructor( private router: Router, private notification: NzNotificationService){
+    // this.notification.info('', '', {nzDuration: 1, nzPauseOnHover: false });
+  }
+  ngOnInit() {
+    this.notification.info('', '', {nzDuration: 1, nzPauseOnHover: false });
+    alert("hello");
   }
   activePath(routePath: string) {
     if (this.route === '') this.route = this.router.url;
