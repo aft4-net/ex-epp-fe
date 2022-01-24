c0df56374629b36f3bfe05dcbfcec8d4d2456802
wolde <woldeyalew04@gmail.com>
2022-01-24 19:51:26 +0300

    to add positions

--- apps/usermanagement/src/app/features/components/userdetails/userdetails.component.ts
+++ apps/usermanagement/src/app/features/components/userdetails/userdetails.component.ts
@@ -13,7 +13,7 @@ import { environment } from '../../../../environments/environment';
 import { AccountService } from '../../../services/user/account.service';
 import { NotificationBar } from '../../../utils/feedbacks/notification';
 import { MessageBar } from '../../../utils/feedbacks/message';
-import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTableFilterList, NzTableModule } from 'ng-zorro-antd/table';
 //import { UserDetailService } from '../../services/user-detail.service';
 import { UserDetail, GroupData } from '../../Models/User/UserDetail';
 import { CustomFormModule } from '../../../shared/modules/forms/custom-form.module';
@@ -21,6 +21,7 @@ import { AuthenticationService } from './../../../../../../../libs/common-servic
 import { PermissionService } from '../../services/permission/permission.service';
 import { PermissionListService } from '../../../../../../../libs/common-services/permission.service';
 import { UserDetailService } from '../../Services/user-detail.service';
+import { IUserModel } from '../../Models/User/UserList';
 
 
 @Component({
@@ -35,13 +36,15 @@ export class UserdetailsComponent implements OnInit {
   loading = false;
   nzSwitch=true;
   isLogin=false;
+  userList : IUserModel[] = [];
+  userListJobTitle : NzTableFilterList=[];
   public userDetals: [UserDetail] | [] = [];
   isRecordUpdated = false;
   selectedRecord: string | undefined;
   cgm=CustomFormModule;
   userId:any;
   thePosition : any;
-  userdetailInfo:any
+  userdetailInfo:any 
   userdetail = new FormGroup({
     UserId: new FormControl(''),
     FullName: new FormControl(''),
@@ -142,7 +145,7 @@ export class UserdetailsComponent implements OnInit {
     this.userDetailService.getUserById(this.userId)
       .subscribe(async (response:any) => {
         this.userdetailInfo = response.Data;
-       
+        this.thePosition = response.Data.userListJobTitle; 
       });
      // this.getPermission();
       //._permissionService.permissionList=this.permissionList;
