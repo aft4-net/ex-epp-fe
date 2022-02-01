0075dfc9887406bdafca2e1ce5fd0fa3b917bf9c
Alemayehu Ewunetu <AEwunetu@excellerentsolutions.com>
2022-02-01 19:59:01 +0300

    hange backend ip and port
    * Changed backend ip and port back to https://3.138.163.97:3030

--- apps/applicant-tracking/src/environments/environment.ts
+++ apps/applicant-tracking/src/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: true,
-  fileUrl:'http://3.138.163.97:3000',
-  apiUrl:'http://3.138.163.97:3000/api/v1',
-  resumeUploadUrl:'http://3.138.163.97:3000/api/upload',
-  photoUploadUrl:'http://3.138.163.97:3000/api/upload'
+  fileUrl:'https://3.138.163.97:3030',
+  apiUrl:'https://3.138.163.97:3030/api/v1',
+  resumeUploadUrl:'https://3.138.163.97:3030/api/upload',
+  photoUploadUrl:'https://3.138.163.97:3030/api/upload'
 };
 
 
