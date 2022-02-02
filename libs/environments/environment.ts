45eaef55fa0469703f3f2e681f560af58a30a2a4
adisalem <adislem@gmail.com>
2022-02-02 23:56:35 +0300

    delete refresh

--- libs/environments/environment.ts
+++ libs/environments/environment.ts
@@ -4,12 +4,14 @@
 
 export const environment = {
   production: false,
-  // apiUrl : 'http://localhost:14696/api/v1',
-  // clientId: '4f5a6105-5df8-4945-941c-ca513b55caab', 
-  // redirectUri: 'http://localhost:4200/'
-  apiUrl: 'https://3.138.163.97:3030/api/v1',
-  clientId: '30cff3d0-c714-4f42-a080-19c5d4ef720e',
-  redirectUri: 'https://18.218.150.53:4200/'
+
+  apiUrl: 'http://localhost:14696/api/v1',
+
+  clientId: '4f5a6105-5df8-4945-941c-ca513b55caab',
+
+  redirectUri: 'http://localhost:4200'
+
+
 };
 
 /*
