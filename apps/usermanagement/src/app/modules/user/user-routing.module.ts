fc53cf4bd2d95248e0cae7e28dfb747712526068
hbelew@excellerentsolutions.com <hbelew@excellerentsolutions.com>
2022-02-04 16:44:10 +0300

    Merge conflicted ..

--- apps/usermanagement/src/app/modules/user/user-routing.module.ts
+++ apps/usermanagement/src/app/modules/user/user-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from '../../components/user/signup/signup.component';
 import { SigninComponent } from '../../components/user/signin/signin.component';
 import { unAuthorizedCheck } from '../../services/autherization/unAuthorizedCheck';
+import { LoginComponent } from '../../features/Account/user/login/login.component';
 
 const routes: Routes = [
   { path: '', component: SigninComponent, canActivate: [unAuthorizedCheck] },
   { path: 'signin', component: SigninComponent, canActivate: [unAuthorizedCheck]},
  
   { path: 'signup', component: SignupComponent, canActivate: [unAuthorizedCheck] },
+
+  { path: 'login', component: LoginComponent, canActivate: [unAuthorizedCheck] },
 ];
 
 @NgModule({
