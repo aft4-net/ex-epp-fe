f2ae489850968b3ad5edff111c3c71a11fc58e3d
fikremariam <fmoges@excellerentsolutions.com>
2022-02-08 09:33:52 +0300

    merge activating-deactivating-user

---
+++ apps/usermanagement/src/app/features/Models/User/user-put.model.ts
@@ -0,0 +1,11 @@
+export interface IUserPutModel {
+    Guid: string;
+    FirstName: string;
+    MiddleName: string;
+    LastName: string;
+    Tel: string;
+    Email: string;
+    UserName: string;
+    Status: string
+  }
+  
