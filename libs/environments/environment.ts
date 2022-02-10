f637ba650bc934518b97dd89b8222d0ea0f698d8
Nathan Daniel <NDaniel@excellerentsolutions.com>
2022-02-10 15:27:36 +0300

    fixed some

--- libs/environments/environment.ts
+++ libs/environments/environment.ts
@@ -10,11 +10,13 @@ export const environment = {
 
   // redirectUri: 'https://epp-fe.excellerentsolutions.com/',
 
+
   apiUrl: 'http://localhost:14696/api/v1',
 
   clientId: '5330d43a-fef4-402e-82cc-39fb061f9b97',
 
   redirectUri: 'http://localhost:4200/'
+
 };
 
 /*
