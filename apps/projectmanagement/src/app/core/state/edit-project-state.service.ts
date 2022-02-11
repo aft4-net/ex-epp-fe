5738a60eae6623fb73edadc438314d06f77c9f9a
Alemayehu Ewunetu <AEwunetu@excellerentsolutions.com>
2022-02-11 14:53:29 +0300

    Update Project Management
    * removed deleteproject function

--- apps/projectmanagement/src/app/core/state/edit-project-state.service.ts
+++ apps/projectmanagement/src/app/core/state/edit-project-state.service.ts
@@ -55,14 +55,6 @@ projectResourceList$=this.projectResourceList.asObservable();
     this.router.navigateByUrl('projectmanagement/edit-project');
   }
 
-  deleteProject(id:string)
-  {
-    this.isOnEditstate=true;
-    this.projectSource.next(data);
-    this. assignResoursceService.getResourceOfProject(data.Guid).subscribe((d:AssignResource[])=>this.updateProjectList(d));
-    this.router.navigateByUrl('projectmanagement/edit-project');
-  }
-
   updateAssignResources()
   {
     this. assignResoursceService.getResourceOfProject( this.projectEditData.Guid).subscribe((d:AssignResource[])=>this.updateProjectList(d));
