fe32ba66ad83d4ac6fad14ae7385aac2dfd52851
Nathan Daniel <NDaniel@excellerentsolutions.com>
2022-02-18 20:44:27 +0300

    css_2

--- apps/resourcemanagement/src/app/Features/Components/employee/family-detail-view/family-detail-view.component.ts
+++ apps/resourcemanagement/src/app/Features/Components/employee/family-detail-view/family-detail-view.component.ts
@@ -67,7 +67,7 @@ export class FamilyDetailViewComponent implements OnInit {
 
   showConfirm(index: number,id:string): void {
     this.confirmModal = this.modalService.confirm({
-      nzTitle: 'Do you want to delete this item?',
+      nzTitle: 'Do you want to delete Family Member ?',
       nzContent: 'The action is not recoverable. ',
       nzOkType: 'primary',
       nzOkText: 'Yes',
