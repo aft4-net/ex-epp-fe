fde221db4f1468bcf8de1f5b10098bc2de9846e5
Nathan Daniel <NDaniel@excellerentsolutions.com>
2022-02-20 00:56:48 +0300

    defaultvalue for prefix

--- apps/resourcemanagement/src/app/Features/Components/employee/emergencycontact-view/emergencycontact-view.component.ts
+++ apps/resourcemanagement/src/app/Features/Components/employee/emergencycontact-view/emergencycontact-view.component.ts
@@ -104,9 +104,7 @@ export class EmergencycontactViewComponent implements OnInit {
       this.form.generateEmergencyContactForm(
         this.form.allEmergencyContacts[index]
       );
-      console.log('this.form.allEmergencyContacts[index]');
-      console.log(this.form.allEmergencyContacts[index]);
-      console.log('this.form.allEmergencyContacts[index]');
+   
     }
   }
 
