14b96e01de83b00705a08c65a934e7c449339b12
isayasone <60773892+isayasone@users.noreply.github.com>
2022-02-21 19:23:56 +0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-  mode:'jit',
- purge: {
-  enabled: process.env.NODE_ENV === 'production',
-  content: [
-  './apps/**/src/**/*.html'
-]},
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
-// ,
+  purge: {
+    enabled: true,
+    content: [
+    './apps/**/*.html',
+    './apps/**/*.js',
+    './libs/**/*.html',
+    './libs/**/*.js'
+  ]},
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  // ,
