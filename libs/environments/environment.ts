a903f7b24f059030d33493420106b84b8b3d7333
Nathan Daniel <NDaniel@excellerentsolutions.com>
2022-03-08 14:59:30 +0300

    isFinal

--- libs/environments/environment.ts
+++ libs/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://epp-be.excellerentsolutions.com/api/v1',
-  clientId: '30cff3d0-c714-4f42-a080-19c5d4ef720e',
-  redirectUri: 'https://epp-fe.excellerentsolutions.com/'
+  //apiUrl: 'https://epp-be.excellerentsolutions.com/api/v1',
+  //clientId: '30cff3d0-c714-4f42-a080-19c5d4ef720e',
+  //redirectUri: 'https://epp-fe.excellerentsolutions.com/'
 
-  // apiUrl: 'http://localhost:14696/api/v1',
-  // clientId: '5330d43a-fef4-402e-82cc-39fb061f9b97',
-  // redirectUri: 'http://localhost:4200/'
+   apiUrl: 'http://localhost:14696/api/v1',
+   clientId: '5330d43a-fef4-402e-82cc-39fb061f9b97',
+   redirectUri: 'http://localhost:4200/'
 };
