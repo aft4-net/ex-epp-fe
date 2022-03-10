48151c046cfe79a09e2f18729bf06833d2346f19
AmanuelZewdu <azewdu87@gmail.com>
2022-03-10 19:13:41 +0300

    fixes

--- apps/clientmanagement/src/app/clients/view-clients/view-clients.component.ts
+++ apps/clientmanagement/src/app/clients/view-clients/view-clients.component.ts
@@ -146,9 +146,8 @@ _commonData.getPermission()
             {
              this.notification.success("Client Deleted Successfully","",{nzPlacement:'bottomRight'}
              );
-             this.
-             initializeData();
-            }
+             this.initializeData();
+            } 
             this.notification.error("You can't delete, this client has projects under it","",{nzPlacement:'bottomRight'});
 
            },
@@ -160,21 +159,6 @@ _commonData.getPermission()
       },
 
       nzCancelText: 'Cancel',
-      nzOnCancel: () => console.log('Cancel'),
-    });
-  }
-
-  showDeleteConfirm(element: any): void {
-    this.modal.confirm({
-      nzTitle: 'Are you sure, you want to cancel this client?',
-      nzContent: '<b style="color: red;"></b>',
-      nzOkText: 'Yes',
-      nzOkType: 'primary',
-      nzOkDanger: true,
-      nzOnOk: () => {
-        this.DeleteClient(element);
-      },
-      nzCancelText: 'No',
       nzOnCancel: () => console.log('Cancel'),
     });
   }
