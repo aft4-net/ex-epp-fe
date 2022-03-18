bb1d481d9a04627bcdfbf4afaaa55dd0723c604a
Biniyam <btechan@excellerentsolutions.com>
2022-03-18 17:27:22 +0300

    group detail fix

--- apps/usermanagement/src/app/features/components/group-detail/group-detail.component.ts
+++ apps/usermanagement/src/app/features/components/group-detail/group-detail.component.ts
@@ -99,7 +99,6 @@ export class GroupDetailComponent implements OnInit {
     this.assinedPermission();
     this._permissionService.getGroupPermissionById(this.groupId);
     this.disableEdit();
-alert("on group detail ngoninit")
   }
   
   disableEdit(){
