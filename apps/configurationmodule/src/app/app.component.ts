9186d1de4a2d9bf1c918325bec2cb71c148fd5a4
Biniyam <btechan@excellerentsolutions.com>
2022-03-21 15:20:21 +0300

    users and configuration selected active tab

--- apps/configurationmodule/src/app/app.component.ts
+++ apps/configurationmodule/src/app/app.component.ts
@@ -16,6 +16,12 @@ export class AppComponent implements OnInit {
   title = 'configurationmodule';
   isLogin = false;
   route = '';
+  departmentIsActive! : boolean;
+  jobTitleIsActive! : boolean;
+  timesheetIsActive! : boolean;
+  countryIsActive! : boolean;
+  dutyStationIsActive! : boolean;
+
   constructor(private router: Router, private notification: NzNotificationService,
     public _commonData: CommonDataService,
     private _authenticationService: AuthenticationService,
@@ -57,19 +63,28 @@ export class AppComponent implements OnInit {
 
   defaultRoute() {
     if (this.authorize(['Create_Department', 'View_Department', 'Update_Department', 'Delete_Department'])) {
+      this.departmentIsActive = true;
       // default route
     } else if (this.authorize(['Create_Job_Title', 'View_Job_Title', 'Update_Job_Title', 'Delete_Job_Title'])) {
-      if(this.router.url === '/configurationmodule')
+      if(this.router.url === '/configurationmodule') {
+        this.jobTitleIsActive = true;
         this.router.navigateByUrl('configurationmodule/job-title');
+      }
     } else if (this.authorize(['Create_Country', 'Delete_Country', 'Update_Country', 'View_Country'])) {
-      if(this.router.url === '/configurationmodule')
+      if(this.router.url === '/configurationmodule') {
+        this.countryIsActive = true;
         this.router.navigateByUrl('configurationmodule/country');
+      }
     } else if (this.authorize(['Create_DutyStation', 'Update_DutyStation', 'Delete_DutyStation', 'View_DutyStation'])) {
-      if(this.router.url === '/configurationmodule')
+      if(this.router.url === '/configurationmodule') {
+        this.dutyStationIsActive = true;
         this.router.navigateByUrl('configurationmodule/duty-station');
+      }
     } else if (this.authorize(['View_Timesheet_Configuration', 'Update_Timesheet_Configuration'])) {
-      if(this.router.url === '/configurationmodule')
+      if(this.router.url === '/configurationmodule') {
+        this.timesheetIsActive = true;
         this.router.navigateByUrl('configurationmodule/timesheet');
+      }
     } else {
       this.router.navigateByUrl('/');
     }
