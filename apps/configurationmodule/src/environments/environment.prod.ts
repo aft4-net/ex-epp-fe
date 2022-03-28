3e8d64d3a9bb91c77268fd9861cc42c2e9e3acdf
Alemayehu Ewunetu <AEwunetu@excellerentsolutions.com>
2022-03-28 19:44:02 +0300

    Production Url Updated

--- apps/configurationmodule/src/environments/environment.prod.ts
+++ apps/configurationmodule/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://epp-be.excellerentsolutions.com/api/v1/',
-  timesheetApiUrl: 'https://epp-be.excellerentsolutions.com/api/v1/TimeSheet/',
-  countryApiUrl: 'https://epp-be.excellerentsolutions.com/api/v1/Country/',
-  dutyStationApiUrl: 'https://epp-be.excellerentsolutions.com/api/v1/DutyBranch/'
+  apiUrl: 'https://api.epp-excellerentsolutions.com/api/v1/',
+  timesheetApiUrl: 'https://api.epp-excellerentsolutions.com/api/v1/TimeSheet/',
+  countryApiUrl: 'https://api.epp-excellerentsolutions.com/api/v1/Country/',
+  dutyStationApiUrl: 'https://api.epp-excellerentsolutions.com/api/v1/DutyBranch/'
 };
