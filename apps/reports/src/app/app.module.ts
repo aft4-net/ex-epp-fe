313f729fe93a23fe4227857f9c468d6a821753b7
wolde <woldeyalew04@gmail.com>
2022-04-12 13:45:18 +0300

    view report components

--- apps/reports/src/app/app.module.ts
+++ apps/reports/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { ViewreportComponent } from './timesheetreports/viewreport/viewreport.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ViewreportComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
---
+++ apps/reports/src/app/timesheetreports/viewreport/viewreport.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'exec-epp-viewreport',
+  templateUrl: './viewreport.component.html',
+  styleUrls: ['./viewreport.component.scss']
+})
+export class ViewreportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
