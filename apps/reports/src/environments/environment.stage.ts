046aae5f68505779d708bb83430478a42e019ae3
Alemayehu Ewunetu <AEwunetu@excellerentsolutions.com>
2022-05-09 20:41:20 +0300

    Fix enviroment variable issue.

--- apps/reports/src/environments/environment.stage.ts
+++ apps/reports/src/environments/environment.stage.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,  
   apiUrl : 'https://epp-be.excellerentsolutions.com/api/v1',
-  redirectUri: 'https://qa.epp-excellerentsolutions.com'
+  redirectUri: 'https://qa.epp-excellerentsolutions.com',  
+  timesheetApiUrl: 'https://qa.epp-excellerentsolutions.com/api/v1/TimeSheet/'
 };
